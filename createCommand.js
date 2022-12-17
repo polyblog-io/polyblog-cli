28403819ab4a46162fa0697d978025cea21b0441
Giorgio Zamparelli <giorgio.zamparelli@gmail.com>
2022-12-17 10:57:10 +0100

    refactor: create commands in separate files

---
+++ createCommand.js
@@ -0,0 +1,20 @@
+/* Copyright 2013 - 2022 Waiterio LLC */
+import path from 'path'
+import git from 'simple-git'
+import commander from 'commander'
+
+function createCommand() {
+  
+  const command = new commander.Command('create')
+  command.action(async (options) => {
+    console.log('create-polyblog')
+    const repoUrl = 'https://github.com/polyblog-io/nextjs-blog-example.git'
+    const repoPath = path.resolve(__dirname, './repo')
+    await git().clone(repoUrl, repoPath, ['--depth', '1'])
+  })
+
+  return command
+  
+}
+
+export default createCommand
--- index.js
+++ index.js
@@ -2,16 +2,13 @@
 /* Copyright 2013 - 2022 Waiterio LLC */
 
 import { program } from 'commander'
-import create from './create.js'
-import download from './download.js'
+import createCommand from './createCommand.js'
+import downloadCommand from './downloadCommand.js'
 
-program.name('polyblog').description('create a blog').version('0.0.1')
-
-program.command('create').action(create)
 program
-  .command('download')
-  .option('-b, --blog <blogId>', 'blogId from polyblog.io')
-  .argument('[directory]', 'directory to download to the blog')
-  .action(download)
+  .description('create a blog')
+  .version('0.0.1')
+  .addCommand(createCommand())
+  .addCommand(downloadCommand())
 
 program.parse(process.argv)
